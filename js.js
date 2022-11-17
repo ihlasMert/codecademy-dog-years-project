6a9a3978726a69f123a4000c13a3c92048bc1613
İHLASMERT <ihlas1mert@gmail.com>
2022-11-17 11:20:51 +0300

    created project

---
+++ js.js
@@ -0,0 +1,24 @@
+var myAge = 26;
+///benim yaşım
+
+let earlyYears = 2; //this line will be change
+
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2
+earlyYears *= 4
+
+console.log(earlyYears)
+console.log(laterYears)
+
+let myAgeInDogYears = earlyYears + laterYears;
+
+
+console.log(myAgeInDogYears)
+let myName = "İhlas Mert".toUpperCase()
+
+console.log(myName)
+let names = `My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.
+`
+
+console.log(names)
